412a45d5872f1cac0ee696cf75d9117b22573fcd
James Chung <jameschung.dream@gmail.com>
2018-08-28 23:13:17 +0800

    First commit

--- Entity.js
+++ Entity.js
@@ -1,7 +1,6 @@
 
-var initPack = { player: [], bullet: [] };
-var removePack = { player: [], bullet: [] };
-
+var initPack = { player: [], bullet: [], enemy: []  };
+var removePack = { player: [], bullet: [], enemy: [] };
 
 
 Entity = function (param) {
@@ -41,20 +40,25 @@ Entity.getFrameUpdateData = function () {
 		initPack: {
 			player: initPack.player,
 			bullet: initPack.bullet,
+			enemy: initPack.enemy,
 		},
 		removePack: {
 			player: removePack.player,
 			bullet: removePack.bullet,
+			enemy: removePack.enemy,
 		},
 		updatePack: {
 			player: Player.update(),
 			bullet: Bullet.update(),
+			enemy: Enemy.update(),
 		}
 	};
 	initPack.player = [];
 	initPack.bullet = [];
+	initPack.enemy = [];
 	removePack.player = [];
 	removePack.bullet = [];
+	removePack.enemy = [];
 	return pack;
 }
 
@@ -91,10 +95,9 @@ Player = function (param) {
 			}else {
 				self.attackInterval++;
 			}
-
-			
-			
 		}
+
+
 	}
 	self.shootBullet = function (angle) {
 		if (Math.random() < 0.1)
@@ -210,6 +213,7 @@ Player.onConnect = function (socket, username) {
 		selfId: socket.id,
 		player: Player.getAllInitPack(),
 		bullet: Bullet.getAllInitPack(),
+		enemy: Enemy.getAllInitPack(),
 	})
 }
 Player.getAllInitPack = function () {
@@ -233,6 +237,119 @@ Player.update = function () {
 	return pack;
 }
 
+//Enemies start
+Enemy = function (param) {
+	var self = Entity(param);
+	self.number = "" + Math.floor(10 * Math.random());
+	self.username = param.username;
+	self.pressingRight = false;
+	self.pressingLeft = false;
+	self.pressingUp = false;
+	self.pressingDown = false;
+	self.pressingAttack = false;
+	self.mouseAngle = 0;
+	self.maxSpd = 10;
+	self.hp = 10;
+	self.hpMax = 10;
+	self.score = 0;
+	self.inventory = new Inventory(param.socket, true);
+
+	self.attackInterval = 0;
+
+	var super_update = self.update;
+	self.update = function () {
+		self.updateSpd();
+
+		super_update();
+
+		if (self.pressingAttack) {
+
+			if (self.attackInterval > 1) {
+				self.shootBullet(self.mouseAngle);
+				self.attackInterval = 0;
+			} else {
+				self.attackInterval++;
+			}
+		}
+
+
+	}
+	self.shootBullet = function (angle) {
+		if (Math.random() < 0.1)
+			self.inventory.addItem("potion", 1);
+		Bullet({
+			parent: self.id,
+			angle: angle,
+			x: self.x,
+			y: self.y,
+			map: self.map,
+		});
+	}
+
+	self.updateSpd = function () {
+
+		self.attackInterval++; //enable attack bar when moving bc idk how to do timers lol
+
+		if (self.pressingRight)
+			self.spdX = self.maxSpd;
+		else if (self.pressingLeft)
+			self.spdX = -self.maxSpd;
+		else
+			self.spdX = 0;
+
+		if (self.pressingUp)
+			self.spdY = -self.maxSpd;
+		else if (self.pressingDown)
+			self.spdY = self.maxSpd;
+		else
+			self.spdY = 0;
+	}
+
+	self.getInitPack = function () {
+		return {
+			id: self.id,
+			x: self.x,
+			y: self.y,
+			number: self.number,
+			hp: self.hp,
+			hpMax: self.hpMax,
+			score: self.score,
+			map: self.map,
+		};
+	}
+	self.getUpdatePack = function () {
+		return {
+			id: self.id,
+			x: self.x,
+			y: self.y,
+			hp: self.hp,
+			score: self.score,
+			map: self.map,
+		}
+	}
+
+	Enemy.list[self.id] = self;
+
+	initPack.enemy.push(self.getInitPack());
+	return self;
+}
+Enemy.list = {};
+Enemy.getAllInitPack = function () {
+	var enemy = [];
+	for (var i in Enemy.list)
+		enemy.push(Enemy.list[i].getInitPack());
+	return enemy;
+}
+Enemy.update = function () {
+	var pack = [];
+	for (var i in Enemy.list) {
+		var enemy = Enemy.list[i];
+		enemy.update();
+		pack.push(enemy.getUpdatePack());
+	}
+	return pack;
+}
+//Enemies end
 
 Bullet = function (param) {
 	var self = Entity(param);
